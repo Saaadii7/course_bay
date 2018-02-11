747870192e35fc6f97c0fbec40f0344451c05527
saaadii7 <saaadii7@gmail.com>
2018-02-12 02:19:51 +0500

    some changes in sound show and documents

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,11 +1,15 @@
 
 function favourite_toggle(image_id){
-	console.log($("#img_"+image_id).prop('checked'),image_id);
+	// console.log($("#img_"+image_id).prop('checked'),image_id);
 	$.ajax({
 	    type:'POST', 
 	    url: '/images/favourite_toggle', 
 	    data: { image_id: image_id, favourite: $("#img_"+image_id).prop('checked')}
-	  }); 
+	 }); 
 	
 }
+function playAudio(audio) {
+ 	console.log(audio)
+    audio.play(); 
+} 
 
