85476ef43a557d7701b5ab7cddc7261e1a02ca32
chtabbii <tayyabarif50@gmail.com>
2018-09-15 16:23:43 +0500

    new user attributes

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,8 @@ ActiveRecord::Schema.define(version: 20180213200402) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -78,8 +79,14 @@ ActiveRecord::Schema.define(version: 20180213200402) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.datetime "dob"
     t.string "gender"
+    t.string "phone_number"
+    t.string "address"
+    t.string "country"
+    t.string "postal_code"
+    t.string "cnic"
     t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
